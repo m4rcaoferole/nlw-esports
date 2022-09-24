b7aa88dffdb5a52b814eeb9d0d0bf8deae70ed15
Marcos Vinicius <marcao.adsistema@gmail.com>
2022-09-24 19:30:45 -0400

    feat importacao/criacao de componentes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native'
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter'
+
+import { Background } from './src/components/Background';
+import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Faaala React Native!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [ fontsLoaded ] = useFonts({ Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <Background>
+     <StatusBar
+      barStyle='light-content'
+      backgroundColor="transparent"
+      translucent
+     />
+     { fontsLoaded ? <Home /> : <Loading /> }
+    </Background>
+  );
+};
---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,22 @@
+import { View, Image } from 'react-native';
+
+import logoImg from '../../assets/logo-nlw-esports.png'
+import { GameCard } from '../../components/GameCard';
+import { Heading } from '../../components/Heading';
+
+import { GAMES } from '../../utils/games';
+import { styles } from './styles';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo}/>
+
+      <Heading title='Encontre seu duo!' subtitle='Selecione o game que deseja jogar...' />
+
+      <GameCard
+        data={GAMES[0]}
+      />
+    </View>
+  );
+}
