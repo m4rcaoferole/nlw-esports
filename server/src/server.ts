4d676fd0334720fd53dec2685034deba769dd9c2
Marcos Vinicius <marcao.adsistema@gmail.com>
2022-10-15 22:17:08 -0400

    feat: casos de uso

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,13 +2,36 @@ import express from 'express'
 
 const app = express()
 
-app.get('/ads', (resquest, response) => {
+// HTTP methods / API RESTful / HTTP Codes
+/**
+ * Query: ...
+ * Route: ...
+ * Body: ...
+ */
+
+app.get('/games', (resquest, response) => {
+  return response.json([]);
+});
+
+app.post('/ads', (resquest, response) => {
+  return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (resquest, response) => {
+  //const gameId = resquest.params.id;
+
   return response.json([
     { id: 1, name: 'Anúncio 1'},
     { id: 2, name: 'Anúncio 2'},
     { id: 3, name: 'Anúncio 3'},
     { id: 4, name: 'Anúncio 4'},
-  ])
-})
+  ]);
+});
+
+app.get('/ads/:id/discord', (resquest, response) => {
+  //const adId = resquest.params.id;
+
+  return response.json([]);
+});
 
 app.listen(3333)
