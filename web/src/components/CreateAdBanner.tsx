e790edd4bbb9bc1a88b4957da3fe492ede2e867f
Marcos Vinicius <marcao.vfo@gmail.com>
2022-11-06 21:17:35 -0400

    feat: form structured

--- web/src/components/CreateAdBanner.tsx
+++ web/src/components/CreateAdBanner.tsx
@@ -1,4 +1,5 @@
 import { MagnifyingGlassPlus } from "phosphor-react";
+import * as Dialog from "@radix-ui/react-dialog"
 
 export function CreateAdsBanner() {
   return (
@@ -13,10 +14,10 @@ export function CreateAdsBanner() {
           </span>
         </div>
 
-        <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+        <Dialog.Trigger className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
           <MagnifyingGlassPlus size={24} />
           Publicar anúncio
-        </button>
+        </Dialog.Trigger>
       </div>
     </div>
   );
