a5e8a2f9cb8532833cd7e6cfefcce9d37202ce30
Marcos Vinicius <marcao.vfo@gmail.com>
2023-01-16 14:41:33 -0400

    feat: criado o cards dos jogos

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,19 +1,22 @@
+import { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Image, TouchableOpacity, View } from "react-native";
-import { useRoute, useNavigation } from '@react-navigation/native'
-import { Entypo } from '@expo/vector-icons'
+import { FlatList, Image, TouchableOpacity, View } from "react-native";
+import { useRoute, useNavigation } from "@react-navigation/native";
+import { Entypo } from "@expo/vector-icons";
 
-import logoImg from '../../assets/logo-nlw-esports.png'
+import logoImg from "../../assets/logo-nlw-esports.png";
 
 import { styles } from "./styles";
 import { THEME } from "../../theme";
 
 import { GameParams } from "../../@types/navigation";
 
+import { DuoCard, DouCardProps } from "../../components/DuoCard";
 import { Background } from "../../components/Background";
 import { Heading } from "../../components/Heading";
 
 export function Game() {
+  const [duos, setDuos] = useState<DouCardProps[]>([]);
 
   const navigation = useNavigation();
   const route = useRoute();
@@ -23,40 +26,49 @@ export function Game() {
     navigation.goBack();
   }
 
+  useEffect(() => {
+    fetch(`http://192.168.100.52:3333/games/${game.id}/ads`)
+      .then((response) => response.json())
+      .then((data) => setDuos(data));
+  }, []);
+
   return (
     <Background>
       <SafeAreaView style={styles.container}>
         <View style={styles.header}>
           <TouchableOpacity onPress={handleGoBack}>
-
             <Entypo
               name="chevron-thin-left"
               color={THEME.COLORS.CAPTION_300}
               size={20}
             />
-
           </TouchableOpacity>
 
-          <Image
-            source={logoImg}
-            style={styles.logo}
-          />
+          <Image source={logoImg} style={styles.logo} />
 
-          <View style={styles.right}/>
+          <View style={styles.right} />
         </View>
 
         <Image
-          source={{ uri: game.bannerUrl}}
+          source={{ uri: game.bannerUrl }}
           style={styles.cover}
           resizeMode="cover"
         />
 
-        <Heading
-          title={game.title}
-          subtitle="Conecte-se e comece a jogar!"
-        />
+        <Heading title={game.title} subtitle="Conecte-se e comece a jogar!" />
 
+        <FlatList
+          data={duos}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <DuoCard data={item} onConnect={() => {}} />
+          )}
+          horizontal
+          style={styles.containerList}
+          contentContainerStyle={styles.contentList}
+          showsHorizontalScrollIndicator={false}
+        />
       </SafeAreaView>
     </Background>
-  )
+  );
 }
